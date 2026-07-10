25c51cef2a78653428f7ab456a4daae60979ca05
talk2sathyajith <talk2sathyajith@gmail.com>
2026-07-10 16:01:26 +0530

    Initial Spring Boot project setup

---
+++ src/test/java/com/sathyajith/digital_banking_system/DigitalBankingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sathyajith.digital_banking_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalBankingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
